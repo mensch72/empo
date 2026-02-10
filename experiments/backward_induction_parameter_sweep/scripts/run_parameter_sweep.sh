bd63ad5d0f8f2649a26eb579e387d741279e8c86
Jobst Heitzig <heitzig@pik-potsdam.de>
2026-02-10 19:25:31 +0100

    hpc script

--- experiments/backward_induction_parameter_sweep/scripts/run_parameter_sweep.sh
+++ experiments/backward_induction_parameter_sweep/scripts/run_parameter_sweep.sh
@@ -7,8 +7,9 @@
 #SBATCH --time=02:00:00
 #SBATCH --ntasks=100
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=5G
 #SBATCH --hint=nomultithread
+#SBATCH --exclusive
 
 # Parameter Sweep HPC Batch Script (Parallel Tasks)
 # 
